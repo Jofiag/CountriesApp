6ce6a60ecabc56c825e0c2c1d1915d4eb05c9d27
Jocelyn AGUEMON <Aguemon.Wahab.97@gmail.com>
2021-10-05 18:17:36 +0200

    Adding annotation in our country model to specify data we are getting from the server.

--- app/src/main/java/com/example/countriesapp/model/Country.java
+++ app/src/main/java/com/example/countriesapp/model/Country.java
@@ -1,8 +1,23 @@
 package com.example.countriesapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Country {
+    /*
+    *   @SerializeName("name") is gson annotation used to tell retrofit that this attribute correspond to the attribute "name" in the server.
+    *   So the "name" inside the parentheses correspond to the server attribute that we want our Country attribute name to be.
+    *
+    *   Notice that if the name you give to the attribute of your object is the same in the server, you don't have to add that annotation,
+    *   but if it's not the case you have to!
+    * */
+
+    @SerializedName("name")
     private String name;
+
+    @SerializedName("capital")
     private String capital;
+
+    @SerializedName("flagPNG")
     private String flag;
 
     public Country(String name, String capital, String flag) {
